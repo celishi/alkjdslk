6f9cf61bac5c93b2f242b534a274fd3171f21031
celia <celiashi666@hotmail.com>
2021-12-28 15:38:59 -0500

    Revert "Spawner, Speed, Slow"
    This reverts commit e2be8d066c0f39664bfbd04ab3cb2d582a750cfb.

--- fat cat/Assets/scripts/Reindeer.cs
+++ fat cat/Assets/scripts/Reindeer.cs
@@ -2,21 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HardCollider : MonoBehaviour
+public class Reindeer : MonoBehaviour
 {
     public int damage;
     public float speed;
-    public float XMin;
 
     // Update is called once per frame
     private void Update()
     {
-        speed = GameObject.FindWithTag("grandma").GetComponent<grandma>().XSpeed;
         transform.Translate(Vector2.left * speed * Time.deltaTime);
-        if (transform.position.x < XMin) 
-        {
-            Destroy(gameObject);
-        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
--- fat cat/Assets/scripts/grandma.cs
+++ fat cat/Assets/scripts/grandma.cs
@@ -9,84 +9,24 @@ public class grandma : MonoBehaviour
     private Vector2 targetPos;
     public float Yincrement;
 
-    public float YSpeed;
+    public float speed;
     public float maxHeight;
     public float minHeight;
 
-    public int health;
-
-    public float DistanceTravelled;
-    public float XSpeed;
-    public float MaxSpeed;
-    private int CurrentLane = 2;
-    public bool slowed = false;
-    private bool SlowedState = false;
-    public float SpeedDecrease;
-    private float SpeedPlaceholder = 0;
-
-    void Start()
-    {
-        DistanceTravelled = 0;
-        targetPos = transform.position;
-    }
+    public int health; 
 
     // Update is called once per frame
     private void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, targetPos, YSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         
         if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
-            CurrentLane--;
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
-            CurrentLane++;
-        }
-        if (slowed == true && SlowedState == true) 
-        {
-            health--;
-            XSpeed = XSpeed - SpeedDecrease;
-            slowed = false;
-        }
-        else if (slowed == true) 
-        {
-            SpeedPlaceholder = XSpeed;
-            XSpeed = XSpeed - SpeedDecrease;
-            SlowedState = true;
-            slowed = false;
-        }
-        if (XSpeed >= SpeedPlaceholder)
-        {
-            SlowedState = false;
-        }
-        if (SlowedState == true) 
-        {
-            XSpeed = XSpeed + (float)0.25 * Time.deltaTime * SpeedDecrease;
-        }
-        if (SlowedState == false) 
-        {
-            XSpeed = Mathf.Pow((float)1.2, (float)0.2 * (Time.time) - 10) + 5;
-            if (XSpeed > MaxSpeed)
-            {
-                XSpeed = MaxSpeed;
-                
-            }
-        }
-        DistanceTravelled = DistanceTravelled + (float)0.01 * XSpeed;
-        if (CurrentLane == 1) 
-        {
-            GetComponent<SpriteRenderer>().sortingLayerID = -1794490669;
-        }
-        if (CurrentLane == 2)
-        {
-            GetComponent<SpriteRenderer>().sortingLayerID = 1916578653;
-        }
-        if (CurrentLane == 3)
-        {
-            GetComponent<SpriteRenderer>().sortingLayerID = -1016285291;
         }
     }
 }
